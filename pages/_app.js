ea7571812b57ca844c19e738c9f91f1bdf913509
LeonelDenett <leoneldenett.dev@hotmail.com>
2022-12-06 17:17:30 +0100

    portfolio dev

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { createTheme, ThemeProvider, responsiveFontSizes } from '@mui/material/styles';
+import "@fontsource/quicksand"
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+// Theme
+let theme = createTheme({
+  typography: {
+    fontFamily: [
+      'quicksand'
+    ].join(','),
+  },
+});
+
+theme = responsiveFontSizes(theme);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
